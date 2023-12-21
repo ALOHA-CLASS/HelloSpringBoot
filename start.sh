04eef04f64cf067bad741691e2ce024886a8ed98
wwwalohacampus <h850415@naver.com>
2023-12-21 21:33:31 +0900

    12312321

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Find the process ID (PID) of the process containing "hello-application.war"
-# pid=$(pgrep -f hello-application.war)
+pid=$(pgrep -f hello-application.war)
 
-# if [ -n "$pid" ]; then
-#   echo "Found process ID: $pid"
-#   echo "Killing process..."
-#   kill "$pid"
-#   echo "Process killed successfully."
-# else
-#   echo "No process found with the specified name."
-# fi
+if [ -n "$pid" ]; then
+  echo "Found process ID: $pid"
+  echo "Killing process..."
+  kill "$pid"
+  echo "Process killed successfully."
+else
+  echo "No process found with the specified name."
+fi
 
 # Change directory to the project
 cd /home/joeun/workspace/HelloSpringBoot
