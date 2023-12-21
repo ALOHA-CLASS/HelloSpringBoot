c1b769c2ed5b6a6a4a0abb5ce89f268b2921d4b7
wwwalohacampus <h850415@naver.com>
2023-12-21 20:06:28 +0900

    init

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Find the process ID (PID) of the process containing "hello-application.war"
+pid=$(pgrep -f hello-application.war)
+
+if [ -n "$pid" ]; then
+  echo "Found process ID: $pid"
+  echo "Killing process..."
+  kill "$pid"
+  echo "Process killed successfully."
+else
+  echo "No process found with the specified name."
+fi
+
+# Change directory to the project
+cd /home/joeun/workspace/HelloSpringBoot
+
+# Pull the latest code from the repository
+git pull
+
+# Build the project
+./gradlew clean build
+
+# Change directory to the build/libs
+cd ./build/libs
+
+# Run the application
+java -jar hello-application.war
