2d80973087b60eccb94b9855525b7dadc019bf1b
wwwalohacampus <h850415@naver.com>
2023-12-22 10:20:39 +0900

    123123

--- start.sh
+++ start.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-# Find the process ID (PID) of the process containing "hello-application.war"
-pid=$(pgrep -f hello-application.war)
+# 저장소
+PROJECT_NAME = hello-application
+REPOSITORY = /home/joeun/workspace/HelloSpringBoot
+DEPLOY_PATH = /home/joeun/workspace/HelloSpringBoot/build/libs
 
-if [ -n $pid ]; then
-  echo "Found process ID: ${pid}"
-  echo "Killing process..."
-  kill $pid
-  echo "Process killed successfully."
-else``
-  echo "No process found with the specified name."
-fi
+# 프로젝트 경로로 이동
+echo "> ${REPOSITORY} 로 이동"
+cd $REPOSITORY
 
-# Change directory to the project
-cd /home/joeun/workspace/HelloSpringBoot
-
-# Pull the latest code from the repository
+# GIT PULL 진행
+echo "> GIT PULL"
 git pull
 
-# Build the project in the background
+# 빌드 시작
+echo "> gradle clean build"
 ./gradlew clean build 
-wait
 
-# Change directory to the build/libs
-cd ./build/libs
+# 현재 실행 중인 서버 pid(프로세스 아이디) 확인
+CURRENT_PID = ${pgrep -f ${PROJECT_NAME}.war}
 
-# Run the application
-nohup java -jar hello-application.war
-# java -jar hello-application.war &
+echo "> 현재 실행 중인 서버 pid : ${CURRENT_PID}"
+
+# 실행 중인 서버 있는지 확인
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 실행 중인 서버 없음"
+else
+  echo "> 현재 실행 중인 서버 종료"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+# 애플리케이션 재배포
+echo "> 새로운 애플리케이션 배포"
+cd $DEPLOY_PATH
+
+nohup java -jar $PROJECT_NAME.war
 
